fd22421489b1d12ac4df25094b1dbc642dfe1cc5
Wr3nch <91681845+wrenchathome@users.noreply.github.com>
2021-10-11 03:39:11 +0200

    Update wrm.sh

--- wrm.sh
+++ wrm.sh
@@ -3,7 +3,6 @@
 #alias watchsync="~/wrm.sh watchsync"
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 if [ $1 == "clearram" ]; then
-  #echo "enter su"
   echo ">> before" && free -m && echo ">> executing sync..." && sync && echo ">> after" && sudo echo 3 > /proc/sys/vm/drop_caches && free -m
 elif [ $1 == "watchsync" ]; then
   watch -d grep  -e Dirty: -e Writeback: /proc/meminfo
